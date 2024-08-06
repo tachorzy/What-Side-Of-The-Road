fc8ac394b3ed1212a5904359267eddef0e035c18
tachorzy <tachorzyad@gmail.com>
2024-08-06 02:53:44 -0500

    feat: added Road.vue component and fixed tailwind configuration

---
+++ src/components/Road.vue
@@ -0,0 +1,8 @@
+<script setup lang="ts">
+</script>
+
+<template>
+      <div class="flex flex-col bg-black w-1/4 h-screen">
+            <h1 class="text-yellow-300 text-xl">test test</h1>
+      </div>
+</template>
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,5 +1,6 @@
 /** @type {import('tailwindcss').Config} */
 export default {
+  purge: ['./src/**/*.{vue,js,ts,jsx,tsx}', './public/index.html'],
   content: [],
   theme: {
     extend: {},
