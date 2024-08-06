b3f44571d1b2f1f30e50bc81fa45428304dd452e
tachorzy <tachorzyad@gmail.com>
2024-08-06 04:30:46 -0500

    feat: added countrydata

--- src/utils/countrydata.ts
+++ src/utils/countrydata.ts
@@ -90,3 +90,19 @@ overwrite([
       name: 'Venezuela'
       }
 ]);
+
+export const filteredISOKeys = Object.keys(getCodeList()).filter(code => 
+            !nonCountries.includes(code)
+      )
+  
+export const isoCodesList = getNameList()
+  
+export const countries = getNames()
+  
+export const filteredCountryKeys = Object.keys(getNameList()).filter(country =>
+      !nonCountries.includes(getNameList()[country].toLowerCase())
+);
+  
+export const filteredCountries = countries.sort().filter(country =>
+      Object.values(filteredCountryKeys).includes(country.toLowerCase())
+)
