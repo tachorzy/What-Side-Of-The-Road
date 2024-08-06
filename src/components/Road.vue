8f6b074e1ca742a9adcf842a5c25da4c222fbd67
tachorzy <tachorzyad@gmail.com>
2024-08-06 03:02:34 -0500

    style: styled Road.vue component

--- src/components/Road.vue
+++ src/components/Road.vue
@@ -1,8 +1,11 @@
 <script setup lang="ts">
+const items = Array.from({ length: 10 });
 </script>
 
 <template>
-      <div class="flex flex-col bg-black w-1/4 h-screen">
-            <h1 class="text-yellow-300 text-xl">test test</h1>
+      <div class="flex flex-col bg-black absolute right-0 top-0 w-2/5 h-screen mr-[12%]">
+            <div class="flex flex-col gap-y-12 py-4">
+                  <div  v-for="item in items" class="self-center justify-center w-4 h-12 bg-yellow-300"></div>
+            </div>
       </div>
 </template>
