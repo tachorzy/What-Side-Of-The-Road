94abd2d32541fa4cacf906ceaf2995ac5a81d2a0
tachorzy <tachorzyad@gmail.com>
2024-08-07 23:12:57 -0500

    feat: added filtering/querying to Combobox

--- src/components/Combobox.vue
+++ src/components/Combobox.vue
@@ -8,11 +8,41 @@
             ComboboxOption,
       } from '@headlessui/vue'
 
-      const selectedCountry = ref('')
-      const query = ref('')
+      interface Country {
+            code: string;
+            name: string;
+      }     
 
+      const selectedCountry = ref<Country | null>(null);
+      const query = ref('')
+      
+      const queriedCountries = computed(() =>
+            query.value === ''
+                  ? filteredCountries
+                  : filteredCountries.filter((country) => {
+                  return country.toLowerCase().includes(query.value.toLowerCase())
+                  })
+      )
 </script>
 
 <template>
+      <Combobox v-model="selectedCountry">
+            <div class="relative mt-1 mr-24">
+                  <div
+                        class="relative w-full cursor-default overflow-hidden rounded-lg bg-white text-left shadow-md focus:outline-none focus-visible:ring-2 focus-visible:ring-white/75 focus-visible:ring-offset-2 focus-visible:ring-offset-teal-300 sm:text-lg"
+                  >
+                        <ComboboxInput @change="query = $event.target.value" class="border-2 border-blue-900  p-2"/>
+                  </div>
 
+                  <ComboboxOptions>
+                  <ComboboxOption
+                        v-for="country in queriedCountries"
+                        :key="country"
+                        :value="country"
+                  >
+                        {{ country }}
+                  </ComboboxOption>
+                  </ComboboxOptions>
+            </div>
+       </Combobox>
 </template>
