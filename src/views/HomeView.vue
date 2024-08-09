6aa8929852b7f5e6a33cac631b3d4a798277824e
tachorzy <tachorzyad@gmail.com>
2024-08-09 02:13:45 -0500

    fix: fixed small syntax bug

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -2,6 +2,7 @@
 import { computed, ref, watch } from 'vue';
 import Combobox from '@/components/Combobox.vue';
 import Road from '@/components/Road.vue';
+
 import { isoCodesList, filteredCountries } from '@/utils/countrydata';
 import { countryTraffic } from '@/utils/trafficdata';
 import { sideOfTheRoad } from '@/state';
@@ -29,8 +30,10 @@ watch(selectedCountry, (newCountry) => {
 <template>
   <main class="">
     <div class="flex flex-col gap-y-8 lg:absolute lg:left-0 lg:top-0 lg:p-40 w-1/2">
-      <h1 v-if="selectedCountry" class="text-gray-700 font-medium text-3xl">{{ selectedCountry }} drives on the <b class="">{{ sideOfTheRoad }}</b> hand side of the road.</h1>
-      <h1 v-else class="text-gray-700 font-medium text-3xl">Welcome.</h1>
+      <div class="flex flex-row gap-x-2">
+        <h1 v-if="selectedCountry" class="text-gray-700 font-medium text-3xl">{{ selectedCountry }} drives on the <b class="">{{ sideOfTheRoad }}</b> hand side of the road.</h1>
+        <h1 v-else class="text-gray-700 font-medium text-3xl">Welcome.</h1>
+      </div>
       <Combobox @country-selected="onCountrySelected" />
     </div>
 
