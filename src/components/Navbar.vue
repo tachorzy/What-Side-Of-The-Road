2f6102b981b9b5a5df1f94beeea8a980c6f901e0
tachorzy <tachorzyad@gmail.com>
2024-08-10 14:25:46 -0500

    feat: added Navbar

---
+++ src/components/Navbar.vue
@@ -0,0 +1,10 @@
+<script setup>
+
+</script>
+
+<template>
+      <div class="flex flex-row gap-x-4 absolute top-0 left-0 p-12 pl-32 align-middle">
+            <img src="/icons/highway.svg" alt="whatsideoftheroad logo" class="w-9 h-9" />
+            <h1 class="text-white font-bold tracking-tighter mt-1.5">WHICH<span class="text-[#dce603] font-bold tracking-tighter">SIDEOFTHEROAD</span>.com</h1>
+      </div>
+</template>
