4791f7adf46aa9077d9264faaec07874e57bbc55
tachorzy <tachorzyad@gmail.com>
2024-08-09 23:28:23 -0500

    feat: added animations to Road.vue

--- src/components/Road.vue
+++ src/components/Road.vue
@@ -16,12 +16,22 @@ watch(sharedSideOfTheRoad, (newValue) => {
 <template>
       <div class="flex flex-col bg-black absolute right-0 top-0 w-2/5 h-screen mr-[12%]">
             <img 
+                  v-if="isRightHand"
+                  v-motion-slide-visible-once-bottom 
                   width="500" 
-                  :class="['mt-96 absolute -rotate-90', isRightHand ? 'right-0 -mr-16' : 'left-0 -ml-16 ']" 
+                  class="mt-96 absolute -rotate-90 right-0 -mr-16" 
                   src="../assets/images/SUV.png"
             />
+            <img 
+                  v-else:
+                  v-motion-slide-visible-once-bottom 
+                  width="500" 
+                  class="mt-96 absolute -rotate-90 left-0 -ml-16" 
+                  src="../assets/images/SUV.png"
+            />
+
             <div class="flex flex-col gap-y-12 py-4">
-                  <div  v-for="item in items" class="self-center justify-center w-4 h-12 bg-yellow-300"></div>
+                  <div  v-for="item in items" class="self-center justify-center w-4 h-12 bg-neutral-200"></div>
             </div>
       </div>
 </template>
--- src/main.ts
+++ src/main.ts
@@ -4,8 +4,11 @@ import { createApp } from 'vue'
 import App from './App.vue'
 import router from './router'
 import './index.css'
+import { MotionPlugin } from '@vueuse/motion'
 
-const app = createApp(App)
+const app = createApp(App);
+
+app.use(MotionPlugin);
 
 app.use(router)
 
