52dab9ecfee97a787bfa67b8d6f5710664ccf91e
tachorzy <tachorzyad@gmail.com>
2024-08-10 18:57:26 -0500

    refactor: created vue components for SUV svgs

--- src/components/Car.vue
+++ src/components/Car.vue
@@ -1,20 +1,21 @@
 <script setup>
+import LeftHandDriveSUV from '@/components/SUV/LeftHandDriveSUV.vue';
+import RightHandDriveSUV from '@/components/SUV/RightHandDriveSUV.vue';
 const props = defineProps({
       isRightHand: Boolean
 });
 </script>
 
 <template>
-      <img 
-            v-if="props.isRightHand"
+      <LeftHandDriveSUV 
+            v-if="props.isRightHand" 
             v-motion-slide-visible-once-bottom 
-            class="mt-60 xl:mt-80 z-10 absolute -rotate-90 right-0 mr-12 md:scale-100 w-28 xl:w-72" 
-            src="/images/SUV_LHD.svg"
+            class="z-10 absolute right-0 mr-12 w-28 xl:w-72" 
       />
-      <img 
-            v-else:
+
+      <RightHandDriveSUV
+            v-else 
             v-motion-slide-visible-once-bottom 
-            class="mt-60 xl:mt-80 z-10 absolute -rotate-90 left-0 ml-12 w-28 xl:w-72" 
-            src="/images/SUV_RHD.svg"
+            class="z-10 absolute left-0 ml-12 w-28 xl:w-72"
       />
 </template>
