00d3653f2639a75bcf4a2dff4f43e629b657292a
tachorzy <tachorzyad@gmail.com>
2024-08-10 20:05:42 -0500

    style: small tweaks for xl screen sizes

--- src/components/SUV/LeftHandDriveSUV.vue
+++ src/components/SUV/LeftHandDriveSUV.vue
@@ -1,7 +1,7 @@
 <template>
       <img 
             v-motion-slide-visible-once-bottom 
-            class="mt-60 xl:mt-80 z-10 absolute -rotate-90 right-0 mr-12 md:scale-100 w-28 xl:w-72" 
+            class="mt-60 xl:mt-80 z-10 absolute -rotate-90 right-0 mr-12 xl:mr-5 2xl:mr-12 w-28 xl:w-56 2xl:w-72" 
             src="/images/SUV_LHD.svg"
       />
 </template>
