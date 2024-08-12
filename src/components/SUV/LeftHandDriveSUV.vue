01127cf21411b7c7e6ce59e8198807f3704c53d1
tachorzy <tachorzyad@gmail.com>
2024-08-12 15:38:56 -0500

    style: tweaked responsiveness on large screens

--- src/components/SUV/LeftHandDriveSUV.vue
+++ src/components/SUV/LeftHandDriveSUV.vue
@@ -1,7 +1,7 @@
 <template>
       <img 
             v-motion-slide-visible-once-bottom 
-            class="mt-60 xl:mt-80 z-10 absolute -rotate-90 right-0 mr-12 md:mr-5 2xl:mr-12 w-28 xl:w-56 2xl:w-72" 
+            class="mt-60 xl:mt-[30%] z-10 absolute right-0 mr-12 md:mr-5 2xl:mr-7 w-28 lg:w-48 xl:w-56 2xl:w-64" 
             src="/images/SUV_LHD.svg"
       />
 </template>
--- src/components/SUV/RightHandDriveSUV.vue
+++ src/components/SUV/RightHandDriveSUV.vue
@@ -1,7 +1,7 @@
 <template>
       <img 
             v-motion-slide-visible-once-bottom 
-            class="mt-60 xl:mt-80 z-10 absolute -rotate-90 left-0 ml-12 md:ml-5 2xl:ml-12 w-28 xl:w-56 2xl:w-72" 
+            class="mt-60 xl:mt-[30%] z-10 absolute left-0 ml-12 md:ml-5 2xl:ml-7 w-28 lg:w-48 xl:w-56 2xl:w-64" 
             src="/images/SUV_RHD.svg"
       />
 </template>
