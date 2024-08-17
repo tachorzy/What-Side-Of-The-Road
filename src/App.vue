dc82facc3c58ed2f868ee56f06017e2308321ddf
tachorzy <tachorzyad@gmail.com>
2024-08-17 13:04:45 -0500

    refactor: renamed NavBar to NavBarItem to fix vercel build error

--- src/App.vue
+++ src/App.vue
@@ -1,10 +1,10 @@
 <script setup lang="ts">
 import { RouterView } from 'vue-router'
-import NavBar from './components/NavBar.vue';
+import NavBarItem from './components/NavBarItem.vue';
 </script>
 
 <template>
-  <NavBar/>
+  <NavBarItem/>
   <RouterView />
 </template>
 
