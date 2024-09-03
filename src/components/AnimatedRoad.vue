43d1cf8f693404b2945b433d8d1e6ea3d7998dce
tachorzy <tachorzyad@gmail.com>
2024-09-03 14:24:16 -0500

    test: added Cypress setup

---
+++ src/components/AnimatedRoad.vue
@@ -0,0 +1,8 @@
+<template>
+    <div class="bg-asphalt-800 absolute w-2/5 self-end right-0 mr-48 flex flex-col gap-y-9 group">
+        <span v-for="index in 18" :key="index" class="bg-neutral-100 w-4 h-16 self-center overflow-y-hidden group-hover:translate-y-full duration-100 animate-bounce"></span>
+    </div>
+</template>
+
+<style>
+</style>
