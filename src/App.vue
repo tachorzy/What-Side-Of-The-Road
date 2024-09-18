dece768336f83f98d8e2a6aba513d1be9bbaaea5
tachorzy <tachorzyad@gmail.com>
2024-09-18 17:39:55 -0500

    chore: added metadata to website header

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,7 @@
 <script setup lang="ts">
 import { RouterView } from 'vue-router'
 import NavBarItem from './components/NavBarItem.vue';
+
 </script>
 
 <template>
--- src/router/index.ts
+++ src/router/index.ts
@@ -7,9 +7,47 @@ const router = createRouter({
     {
       path: '/',
       name: 'home',
-      component: HomeView
+      component: HomeView,
+      meta: {
+        title: 'Which Side of the Road?',
+        metaTags: [
+          {
+            name: 'description',
+            content: 'Find if a country drives on the left or the right.'
+          },
+          {
+            property: 'og:description',
+            content: 'Find if a country drives on the left or the right.'
+          }
+        ]
+      }
     },
   ]
 })
+
+
+router.beforeEach((to, from, next) => {
+  if (to.meta.title) {
+    document.title = to.meta.title as string;
+  }
+
+  Array.from(document.querySelectorAll('[data-vue-router-controlled]')).forEach(el => el.parentNode?.removeChild(el));
+
+  if (to.meta.metaTags) {
+    (to.meta.metaTags as Array<{ name?: string; property?: string; content: string }>).forEach(tagDef => {
+      const tag = document.createElement('meta');
+
+      Object.keys(tagDef).forEach(key => {
+        tag.setAttribute(key, tagDef[key as keyof typeof tagDef] as string);
+      });
+
+      tag.setAttribute('data-vue-router-controlled', '');
+
+      document.head.appendChild(tag);
+    });
+  }
+
+  next();
+});
 
 export default router
